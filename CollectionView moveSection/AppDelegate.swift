37c7d2cb0cba86b391636723710dbccc4f9bff2c
David Nadoba <dnadoba@gmail.com>
2019-01-24 07:09:22 +0100

    Initial Commit

---
+++ CollectionView moveSection/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CollectionView moveSection
+//
+//  Created by David Nadoba on 24.01.19.
+//  Copyright © 2019 David Nadoba. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CollectionView moveSection/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CollectionView moveSection
+//
+//  Created by David Nadoba on 24.01.19.
+//  Copyright © 2019 David Nadoba. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
